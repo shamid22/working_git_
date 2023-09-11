484e8b69c802cd52d7f6bb70325a2334b97c46dc
shamid22 <shamid2@unl.edu>
2023-09-11 13:16:14 -0700

    intial commit

--- projit.R
+++ projit.R
@@ -1,3 +1,4 @@
-usethis::use_git()## make condti between githup and windoses 
+usethis::use_github()## make condti between githup and windoses 
 #use_git_ignore(".DS_Store")
 usethis::use_readme_md()
+usethis::use_github()## connect local repo to github
